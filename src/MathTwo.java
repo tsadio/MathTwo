a34da4f394f4a8239bbf6cce8e32b18800e51bcf
tsadio <tsadio@montgomerycollege.edu>
2020-04-22 16:06:10 -0400

    first commit

---
+++ src/MathTwo.java
@@ -0,0 +1,8 @@
+public class MathTwo {
+    public static void main (String [] args ) {
+        int myNumber = 2;
+        double myOtherNumber = 1.7938;
+        System.out.println("My Number = " + myNumber);
+        System.out.println("My Other Number = " + myOtherNumber);
+    }
+}
